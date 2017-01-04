e3e5a6bdc5d368015c72ce89f9209e2e3e422e41
HTT <1554568135@qq.com>
2017-01-04 14:32:09 +0800

    查询货主、司机信息

--- src/com/controller/InfoController.java
+++ src/com/controller/InfoController.java
@@ -1,13 +1,21 @@
 package com.controller;
 
 import com.model.CarInfo;
+import com.model.OwnerInfo;
+import com.model.User;
 import com.service.InfoService;
+import com.sun.xml.internal.bind.unmarshaller.InfosetScanner;
 import com.utils.CookieUtils;
+import org.apache.ibatis.annotations.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -16,15 +24,51 @@ public class InfoController {
     @Resource
     private InfoService infoService;
 
-    public String fillInCarInfo(CarInfo carInfo, HttpServletRequest request){
+    @ResponseBody
+    @RequestMapping("/fillInCarInfo")
+    public String fillInCarInfo(CarInfo carInfo, HttpServletRequest request) {
 
-        String id= CookieUtils.getCookieValueByName(request,"userId");
-        if (null==id){
+        String id = CookieUtils.getCookieValueByName(request, "userId");
+        if (null == id) {
             return "false";
-        }else {
-           // carInfo.setId();
+        } else {
+            return infoService.insertCarInfo(carInfo) + "";
         }
-
     }
 
+    @ResponseBody
+    @RequestMapping("/querycarInfo")
+    public Map<String, Object> querycarInfo(HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        String id=CookieUtils.getCookieValueByName(request, "userId");
+        if(null == id) {
+            map.put("reason", "用户不存在");
+        } else {
+            map = infoService.getCarInfo(Integer.parseInt(id));
+            map.put("result", "true");
+            return map;
+        }
+        map.put("result", "false");
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("/queryownerInfo")
+    public Map<String, Object> queryownerInfo(HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        String id=CookieUtils.getCookieValueByName(request, "userId");
+        if(null == id) {
+            map.put("reason", "用户不存在");
+        } else {
+            map = infoService.getOwnerInfo(Integer.parseInt(id));
+            map.put("result", "true");
+            return map;
+        }
+        map.put("result", "false");
+        return map;
+    }
+
+
 }
+
+
