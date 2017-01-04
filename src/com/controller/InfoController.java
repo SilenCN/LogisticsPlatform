061697d2410add2313de251828e031f2ce35c447
HTT <1554568135@qq.com>
2017-01-04 14:38:05 +0800

    查询货主、司机信息

--- src/com/controller/InfoController.java
+++ src/com/controller/InfoController.java
@@ -5,9 +5,11 @@ import com.service.InfoService;
 import com.utils.CookieUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -16,15 +18,36 @@ public class InfoController {
     @Resource
     private InfoService infoService;
 
-    public String fillInCarInfo(CarInfo carInfo, HttpServletRequest request){
-
-        String id= CookieUtils.getCookieValueByName(request,"userId");
-        if (null==id){
-            return "false";
-        }else {
-           // carInfo.setId();
+    @ResponseBody
+    @RequestMapping("/querycarInfo")
+    public Map<String, Object> querycarInfo(HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        String id=CookieUtils.getCookieValueByName(request, "userId");
+        if(null == id) {
+            map.put("reason", "用户不存在");
+        } else {
+            map = infoService.getCarInfo(Integer.parseInt(id));
+            map.put("result", "true");
+            return map;
         }
+        map.put("result", "false");
+        return map;
+    }
 
+    @ResponseBody
+    @RequestMapping("/queryownerInfo")
+    public Map<String, Object> queryownerInfo(HttpServletRequest request){
+        Map<String, Object> map = new HashMap<>();
+        String id=CookieUtils.getCookieValueByName(request, "userId");
+        if(null == id) {
+            map.put("reason", "用户不存在");
+        } else {
+            map = infoService.getOwnerInfo(Integer.parseInt(id));
+            map.put("result", "true");
+            return map;
+        }
+        map.put("result", "false");
+        return map;
     }
 
 }
