813787829ff8b0bd3e9ca40273a2556760b43932
1039795376@qq.com <li2302838307>
2018-01-04 09:51:52 +0800

    完成查询信息的SQL、DAO

---
+++ src/com/model/OwnerInfo.java
@@ -0,0 +1,40 @@
+package com.model;
+
+public class OwnerInfo {
+    private int id;
+    private String phone;
+    private String address;
+    private int goods;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getGoods() {
+        return goods;
+    }
+
+    public void setGoods(int goods) {
+        this.goods = goods;
+    }
+}
