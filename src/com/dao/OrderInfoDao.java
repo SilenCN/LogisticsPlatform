872ab25b204db8eb9c1550153aed531d633c593e
1039795376@qq.com <li2302838307>
2018-01-09 20:36:34 +0800

    修改模板

--- src/com/dao/OrderInfoDao.java
+++ src/com/dao/OrderInfoDao.java
@@ -1,6 +1,7 @@
 package com.dao;
 
 import com.model.OrderInfo;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
@@ -10,19 +11,19 @@ public interface OrderInfoDao {
 
     int deleteOrderInfo(int id);
 
-    List<Map<String , Object>> getOrderInfo(int page);
+    List<Map<String , Object>> getOrderInfo(int start);
 
-    List<Map<String , Object>> selectOrderInfo(int type, String departure, String target,int page);
+    List<Map<String , Object>> selectOrderInfo(@Param("type") int type,@Param("departure") String departure,@Param("target") String target, @Param("start") int start);
 
-    List<Map<String , Object>> selectOrderInfoExpType(String departure, String target,int page);
+    List<Map<String , Object>> selectOrderInfoExpType(String departure, String target,int start);
 
-    List<Map<String , Object>> selectOrderInfoExpDeparture (int type, String target,int page);
+    List<Map<String , Object>> selectOrderInfoExpDeparture (int type, String target,int start);
 
-    List<Map<String , Object>> selectOrderInfoExpTarget (int type, String departure, int page);
+    List<Map<String , Object>> selectOrderInfoExpTarget (int type, String departure, int start);
 
-    List<Map<String , Object>> selectOrderInfoByTarget (String target,int page);
+    List<Map<String , Object>> selectOrderInfoByTarget (String target,int start);
 
-    List<Map<String , Object>> selectOrderInfoByType (int type, int page);
+    List<Map<String , Object>> selectOrderInfoByType (int type, int start);
 
-    List<Map<String , Object>> selectOrderInfoByDeparture (String departure, int page);
+    List<Map<String , Object>> selectOrderInfoByDeparture (String departure, int start);
 }
